72a71f8416175aec0cbd25baa82b9b18c6f20c74
jerome-montino <jerome.montino@gmail.com>
2018-11-23 17:57:59 +0800

    Init commit of setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='polyhash',
+	version='1.0.0',
+	description='Python library for converting polygons to geohashes and vice versa.',
+	author='Jerome Montino',
+    author_email='jerome.montino@gmail.com',
+    url='https://github.com/jerome-montino/polyhash'
+)
