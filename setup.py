c9df7ab6217d1beaed4fd212647902e88539847d
jerome-montino <jerome.montino@gmail.com>
2018-11-23 18:24:45 +0800

    Fixed an issue with install_requires

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+install_requires = ['shapely', 'python-geohash', 'georaptor']
+
 setup(
     name='polyhash',
 	version='1.0.0',
 	description='Python library for converting polygons to geohashes and vice versa.',
 	author='Jerome Montino',
     author_email='jerome.montino@gmail.com',
-    url='https://github.com/jerome-montino/polyhash'
+    url='https://github.com/jerome-montino/polyhash',
+    license='MIT',
+    install_requires=install_requires
 )
