df00f61416c9daf934705565e2747dfeaa6a16f1
Jerome Montino <jerome.montino@gmail.com>
2018-11-26 15:18:05 +0800

    Adjusted version and setup parameters for new install.

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ install_requires = ['Shapely', 'python-geohash', 'georaptor']
 
 setup(
     name='python-polyhash',
-	version='0.1.0',
+	version='0.3.0',
 	description='Python library for converting polygons to geohashes and vice versa.',
 	author='Jerome Montino',
     author_email='jerome.montino@gmail.com',
     url='https://github.com/jerome-montino/polyhash',
     license='MIT',
+    py_modules=['polyhash'],
     install_requires=install_requires
 )
